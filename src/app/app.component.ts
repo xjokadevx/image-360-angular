29e0e49d6a41799df03a660f6f015dde03af9cd4
Jose Medellin <kabo.0699@gmai.com>
2024-07-03 13:45:16 -0600

    init modeling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,8 +6,32 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'image-360-angular';
+
+  imageUrls = [
+    './../../src/assets/img/1.png',
+    'https://example.com/image2.jpg',
+    'https://example.com/image3.jpg',
+    // Add more image URLs here
+  ];
+
+  currentImageIndex = 0;
+
+  get currentImageUrl() {
+    return this.imageUrls[0];
+  }
+
+  prevImage() {
+    this.currentImageIndex =
+      (this.currentImageIndex - 1 + this.imageUrls.length) %
+      this.imageUrls.length;
+  }
+
+  nextImage() {
+    this.currentImageIndex =
+      (this.currentImageIndex + 1) % this.imageUrls.length;
+  }
 }
